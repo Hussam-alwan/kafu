7318c330962a32814e52572dcd17684660e3bfa8
AbdulHadi Assi <178458269+abdulhadi-assi@users.noreply.github.com>
2025-04-15 15:12:04 +0300

    initial commit

---
+++ src/main/java/com/kafu/kafu/KafuApplication.java
@@ -0,0 +1,13 @@
+package com.kafu.kafu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafu/kafu/KafuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafu.kafu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

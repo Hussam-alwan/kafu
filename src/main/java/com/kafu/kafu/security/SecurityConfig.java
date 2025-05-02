69425464023c4e181f8b91d2e782691286986222
AbdulHadi Assi <178458269+abdulhadi-assi@users.noreply.github.com>
2025-05-02 16:50:58 +0300

    updating security config

--- src/main/java/com/kafu/kafu/security/SecurityConfig.java
+++ src/main/java/com/kafu/kafu/security/SecurityConfig.java
@@ -10,11 +10,13 @@ import org.springframework.security.config.annotation.web.configurers.CsrfConfig
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
+//import org.springframework.web.cors.reactive.CorsConfigurationSource;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 import java.util.List;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -27,13 +29,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(withDefaults())
                 .csrf(CsrfConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/login").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**",
-            "/swagger-resources/**",
-            "/webjars/**",
-            "/api-docs/**").permitAll()
+                                "/swagger-resources/**",
+                                "/webjars/**",
+                                "/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
@@ -54,11 +57,9 @@ public class SecurityConfig {
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-User-ID"));
         config.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**", config);
-        source.registerCorsConfiguration("/v3/api-docs", config);
-        source.registerCorsConfiguration("/v3/api-docs/**", config);
-        source.registerCorsConfiguration("/swagger-ui/**", config);
+        source.registerCorsConfiguration("/**", config);
         return source;
     }
 }
